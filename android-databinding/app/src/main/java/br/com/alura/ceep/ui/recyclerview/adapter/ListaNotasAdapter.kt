4ededb2785bdac4f2b3808a8eba59c2b3c4a17c9
Marcos Prado <marcosprprado@yahoo.com.br>
2022-02-11 19:29:00 -0300

    new features: add data binding, livedata with data binding

--- android-databinding/app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.kt
+++ android-databinding/app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.kt
@@ -2,19 +2,16 @@ package br.com.alura.ceep.ui.recyclerview.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
-import android.view.View.GONE
-import android.view.View.VISIBLE
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import br.com.alura.ceep.R
+import br.com.alura.ceep.databinding.ItemNotaBinding
 import br.com.alura.ceep.model.Nota
-import br.com.alura.ceep.ui.extensions.carregaImagem
-import kotlinx.android.synthetic.main.item_nota.view.*
+import br.com.alura.ceep.ui.databinding.NotaData
 
 class ListaNotasAdapter(
     private val context: Context,
@@ -22,8 +19,21 @@ class ListaNotasAdapter(
 ) : ListAdapter<Nota, ListaNotasAdapter.ViewHolder>(DiffCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val viewCriada = LayoutInflater.from(context).inflate(R.layout.item_nota, parent, false)
-        return ViewHolder(viewCriada)
+        val inflater = LayoutInflater.from(context)
+        val viewDataBinding = ItemNotaBinding.inflate(inflater, parent, false)
+        return ViewHolder(viewDataBinding).also {
+            viewDataBinding.lifecycleOwner = it
+        }
+    }
+
+    override fun onViewAttachedToWindow(holder: ViewHolder) {
+        super.onViewAttachedToWindow(holder)
+        holder.marcaComoAtivo()
+    }
+
+    override fun onViewDetachedFromWindow(holder: ViewHolder) {
+        super.onViewDetachedFromWindow(holder)
+        holder.marcaComoDesativado()
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -32,57 +42,43 @@ class ListaNotasAdapter(
         }
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(private val viewDataBinding: ItemNotaBinding) :
+        RecyclerView.ViewHolder(viewDataBinding.root), LifecycleOwner {
 
         private lateinit var nota: Nota
-        private val campoTitulo: TextView by lazy {
-            itemView.item_nota_titulo
-        }
-        private val campoDescricao: TextView by lazy {
-            itemView.item_nota_descricao
-        }
-        private val campoFavorita: ImageView by lazy {
-            itemView.item_nota_favorita
-        }
-        private val campoImagem: ImageView by lazy {
-            itemView.item_nota_imagem
-        }
+        private val registry = LifecycleRegistry(this)
 
         init {
-            itemView.setOnClickListener {
-                if (::nota.isInitialized) {
-                    onItemClickListener(nota)
-                }
+            viewDataBinding.listener = this
+            registry.markState(Lifecycle.State.INITIALIZED)
+        }
+
+        override fun getLifecycle(): Lifecycle = registry
+
+        fun marcaComoAtivo() {
+            registry.markState(Lifecycle.State.STARTED)
+        }
+
+        fun marcaComoDesativado() {
+            registry.markState(Lifecycle.State.DESTROYED)
+        }
+
+        fun clicaNaNota() {
+            if (::nota.isInitialized) {
+                onItemClickListener(nota)
             }
         }
 
         fun vincula(nota: Nota) {
             this.nota = nota
-            campoTitulo.text = nota.titulo
-            campoDescricao.text = nota.descricao
-            if (this.nota.favorita) {
-                campoFavorita.visibility = VISIBLE
-            } else {
-                campoFavorita.visibility = GONE
-            }
-            campoImagem.carregaImagem(nota.imagemUrl)
-            if (nota.imagemUrl.isEmpty()) {
-                campoImagem.visibility = GONE
-            } else {
-                campoImagem.visibility = VISIBLE
-            }
+            val notaData = NotaData(nota)
+            viewDataBinding.nota = notaData
         }
-
     }
 
 }
 
 object DiffCallback : DiffUtil.ItemCallback<Nota>() {
-    override fun areItemsTheSame(
-        oldItem: Nota,
-        newItem: Nota
-    ) = oldItem.id == newItem.id
-
+    override fun areItemsTheSame(oldItem: Nota, newItem: Nota) = oldItem.id == newItem.id
     override fun areContentsTheSame(oldItem: Nota, newItem: Nota) = oldItem == newItem
-
 }
